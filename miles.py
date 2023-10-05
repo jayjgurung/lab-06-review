977edfb797bc1331e224b6374fca3510d87ef91a
jayjgurung <newuser@Jxxded-MBP.lan1>
2023-10-04 20:24:28 -0400

    added miles and temperature, both .html and .py

---
+++ miles.py
@@ -0,0 +1,5 @@
+print("Milage Calculator")
+mile = input("How many miles did you drive?\n")
+gallon = input("How many gallons of gas did you use?\n")
+milage = float(mile)/float(gallon)
+print("Your car gets "+ str(milage) +" per gallon.")
---
+++ temperature.py
@@ -0,0 +1,4 @@
+print("Temperature conversion Celcius to Fahrenheit!")
+celcius = input("Enter The Temp in Celcius:\n")
+fahrenheit = float(celcius) * 1.8 + 32
+print(celcius +" degree Celcius is "+ str(fahrenheit)+" degree in Fahrenheit!")
